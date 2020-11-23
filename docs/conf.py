04f191f43ff98bd435b74a99878e7a946540d688
Rodolfo Edelmann <rudicba@gmail.com>
2020-11-23 20:56:02 -0300

    fix rtd not compatible with sphinx_autodoc_typehints

--- docs/conf.py
+++ docs/conf.py
@@ -27,12 +27,7 @@ author = "Rodolfo E. Edelmann"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-    "recommonmark",
-    "sphinx.ext.autodoc",
-    "sphinx.ext.napoleon",
-    "sphinx_autodoc_typehints",
-]
+extensions = ["recommonmark", "sphinx.ext.autodoc", "sphinx.ext.napoleon"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
